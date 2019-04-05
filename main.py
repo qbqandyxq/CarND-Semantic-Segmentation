cf3ca8d3cf012398714ad1e9dcfefa169a921c72
mwx <maowx2017@gmail.com>
2019-04-05 22:23:11 +0900

    finish

--- main.py
+++ main.py
@@ -36,12 +36,12 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    tf.save_model.loader.load(sess, [vgg_tag],vgg_tag)
+    tf.saved_model.loader.load(sess, [vgg_tag],vgg_path)
     graph = tf.get_default_graph()
     w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
     keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
     w3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
-    w4 = graph.get_tensor_by_namevgg_layer4_out_tensor_name
+    w4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
     w7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     return w1, keep, w3, w4, w7
 tests.test_load_vgg(load_vgg, tf)
@@ -63,7 +63,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     pool4_out = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     #skip connection
     pool4_out = tf.add(pool4_out, pool5_outx2)
-    pool4_outx2= tf.layers.conv2d(pool4_out, num_classes, 4, 2,padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    pool4_outx2= tf.layers.conv2d_transpose(pool4_out, num_classes, 4, 2,padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     #layers3
     
     pool3_out= tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
@@ -85,7 +85,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     # TODO: Implement function
     logits=tf.reshape(nn_last_layer, (-1, num_classes))
     correct_label = tf.reshape(correct_label,(-1, num_classes))
-    cross_entropy_loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logit(logits= logits, labels= correct_label))
+    cross_entropy_loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= logits, labels= correct_label))
     optimizer = tf.train.AdamOptimizer(learning_rate)
     train_op = optimizer.minimize(cross_entropy_loss)
     
@@ -113,7 +113,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for epoch in range(epochs):
         print("Training in epoch {}".format(epoch +1))
         for image, label in get_batches_fn(batch_size):
-             _,loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image:image, correct_label:lable,keep_prob: kp, learning_rate: lr})
+             _,loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image:image, correct_label:label,keep_prob: 0.5, learning_rate: 1e-3})
              print("The loss is {:.3f}".format(loss))
     print("over")
              
@@ -138,10 +138,12 @@ def run():
     # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
-    lr=1e-3
-    kp=0.5
+    
+   
     epochs=100
     batch_size=32
+    kp=0.5
+    lr=1e-3
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
@@ -154,10 +156,9 @@ def run():
         # TODO: Build NN using load_vgg, layers, and optimize function
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
         learning_rate = tf.placeholder(tf.float32, name='learning_rate')
-        
         input_image, keep_prob, layer3_out, layer4_out, layer7_out=load_vgg(sess, vgg_path)
-        layer_output=layers(layer3_out, layer4_out, layer7_out)
-        logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
+        layer_output=layers(layer3_out, layer4_out, layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(layer_output, correct_label, learning_rate, num_classes)
         # TODO: Train NN using the train_nn function
         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
                  correct_label, keep_prob, learning_rate)
